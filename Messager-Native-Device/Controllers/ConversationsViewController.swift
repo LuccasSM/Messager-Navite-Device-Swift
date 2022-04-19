32cf6a18ef17ff5d0e4aff26589303b115ceffa1
Luccas <mathaluc2016@outlook.com>
2022-04-18 22:54:51 -0300

    aula 3 do curso

--- Messager-Native-Device/Controllers/ConversationsViewController.swift
+++ Messager-Native-Device/Controllers/ConversationsViewController.swift
@@ -11,5 +11,19 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true) // insert false
+        }
     }
 }
